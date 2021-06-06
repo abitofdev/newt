b7c6efcbc188c0d7a8def6bc44059871135f3de6
Ashley <7422565+abitofdev@users.noreply.github.com>
2021-06-06 15:08:23 +0100

    Add Tauri

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NativeBridgeService } from './modules/global/native-bridge.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'newt';
+
+  constructor(private readonly bridge: NativeBridgeService) {
+    document.addEventListener('contextmenu', (event) => event.preventDefault());
+  }
+
+  ngOnInit() {
+    console.time('get files');
+    this.bridge.readDir('../').then((dirContents) => {
+      console.timeEnd('get files');
+      console.log(dirContents.map((d) => d.name));
+    });
+  }
 }
