020d491f92e02e3ae150317891f9fbe663659c89
Connor Butcher <63259295+connorbutcher@users.noreply.github.com>
2021-06-20 19:54:25 +0100

    Note bag expand collapse changes.

--- src/app/modules/notes-explorer/notes-explorer.component.ts
+++ src/app/modules/notes-explorer/notes-explorer.component.ts
@@ -1,9 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { FileEntry } from '@tauri-apps/api/fs';
 import { NativeBridgeService } from '../global/native-bridge.service';
+import { v4 as uuidv4 } from 'uuid';
 
 interface NestedFileEntry extends FileEntry {
+  id: string;
+  parentId?: string;
+
   level: number;
+
+  hasChildren: boolean;
+  isExpanded: boolean;
 }
 
 @Component({
@@ -13,16 +20,49 @@ interface NestedFileEntry extends FileEntry {
 })
 export class NotesExplorerComponent implements OnInit {
   private _contents: NestedFileEntry[] = [];
+  private _fileEntries: NestedFileEntry[] = [];
 
   constructor(private readonly _bridge: NativeBridgeService) {}
 
   ngOnInit(): void {
     this._bridge.readDir('../src', { recursive: true }).then((files) => {
-      this._contents = this.buildStructureLevels(files);
+      this._fileEntries = this.buildStructureLevels(files);
+      this._contents = JSON.parse(JSON.stringify(this._fileEntries));
+
+      //Only show top level items to start with.
+      this._contents = this._contents.filter((c) => !c.parentId);
     });
   }
 
-  private buildStructureLevels(files?: FileEntry[], level: number = 1): NestedFileEntry[] {
+  public shouldShowNode(node: NestedFileEntry): boolean {
+    if (node.parentId) {
+      const directParent = this.contents.find((nfe) => nfe.id === node.parentId);
+      const topParent = this.findTopLevelParent(node);
+
+      if (topParent && directParent) {
+        return topParent.isExpanded && directParent.isExpanded;
+      }
+
+      return true;
+    }
+
+    return true;
+  }
+
+  public expand(node: NestedFileEntry): void {
+    node.isExpanded = !node.isExpanded;
+
+    const nodes = this._fileEntries.filter((fe) => fe.parentId && fe.parentId === node.id);
+    const index = this._contents.findIndex((c) => c.id === node.id);
+
+    if (!node.isExpanded) {
+      this._contents.splice(index + 1, nodes.length);
+    } else {
+      this._contents.splice(index + 1, 0, ...nodes);
+    }
+  }
+
+  private buildStructureLevels(files?: FileEntry[], level: number = 1, parentId?: string): NestedFileEntry[] {
     const result: NestedFileEntry[] = [];
 
     if (!files) {
@@ -30,11 +70,35 @@ export class NotesExplorerComponent implements OnInit {
     }
 
     for (const file of files) {
-      result.push({ ...file, level });
-      result.push(...this.buildStructureLevels(file.children, level + 1));
+      const id = uuidv4();
+
+      result.push({
+        id,
+        ...file,
+        level,
+        hasChildren: file.children && file.children.length > 0 ? true : false,
+        parentId,
+        isExpanded: false,
+      });
+
+      result.push(...this.buildStructureLevels(file.children, level + 1, id));
     }
 
     return result;
+  }
+
+  private findTopLevelParent(node: NestedFileEntry): NestedFileEntry {
+    if (node.parentId) {
+      let parent = this._contents.find((nfe) => nfe.id === node.parentId);
+
+      if (parent) {
+        parent = this.findTopLevelParent(parent);
+
+        return parent;
+      }
+    }
+
+    return node;
   }
 
   public get contents(): NestedFileEntry[] {
